ef3f62cdaa241c461216beff91294aaff8a54094
ffespo <ffesposti10@gmail.com>
2025-05-16 19:57:02 +0200

    commit Cercare elementi nel DOM

---
+++ JavascriptAdanced/CercareElementiDOM.js
@@ -0,0 +1,11 @@
+// 1. Tutti gli elementi con classe "paragrafo"
+const paragrafi = document.querySelectorAll('.paragrafo');
+console.log('Paragrafi:', paragrafi);
+
+// 2. Elemento con id "elemento-3"
+const elemento3 = document.getElementById('elemento-3');
+console.log('Elemento con ID "elemento-3":', elemento3);
+
+// 3. Tutti gli elementi <h2>
+const intestazioniH2 = document.querySelectorAll('h2');
+console.log('Elementi h2:', intestazioniH2);
